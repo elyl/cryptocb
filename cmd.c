3021788910433047512b85a67ac4a26421c7a164
elyl <elylll@gmail.com>
2016-10-26 13:28:51 +0200

    Corrected show_all so that it can work

--- cmd.c
+++ cmd.c
@@ -40,18 +40,20 @@ void cmd_add(char **args)
   memcpy(new_entry.pin, args[3], 3);
   //TODO: Verifier le bon formatage des donnees d'entree
   AES_set_encrypt_key(master_key, KEY_SIZE, &key);
-  AES_cbc_encrypt((unsigned char*)&new_entry, out_buffer, sizeof(t_entry), &key, iv, KEY_SIZE);
+  AES_cbc_encrypt((unsigned char*)&new_entry, out_buffer, sizeof(t_entry), &key, iv, AES_ENCRYPT);
   write(secret_file, out_buffer, sizeof(t_entry));
 }
 
 void cmd_show(char **args)
 {
+  args = args;
   print_tab(args);
   printf("show not yet implemented\n");
 }
 
 void cmd_exit(char **args)
 {
+  args = args;
   memset(&master_key, 0, sizeof(AES_KEY));
   exit(0);
 }
@@ -59,19 +61,22 @@ void cmd_exit(char **args)
 void cmd_show_all(char **args)
 {
   unsigned char	buffer[BUFFER_SIZE];
+  unsigned char iv[AES_BLOCK_SIZE];
   int		n;
   union u_entry	entry;
   AES_KEY	key;
 
-  lseek(secret_file, -BUFFER_SIZE, SEEK_END);
+  args = args;
+  memset(iv, 0, AES_BLOCK_SIZE);
+  AES_set_decrypt_key(master_key, MASTER_KEY_SIZE, &key);
+  lseek(secret_file, 0, SEEK_SET);
   while ((n = read(secret_file, buffer, BUFFER_SIZE)) != 0)
     {
-      while (n > 0)
+      while (n < BUFFER_SIZE)
 	{
-	  n -= ENTRY_SIZE;
-	  AES_set_decrypt_key(master_key, MASTER_KEY_SIZE, &key);
-	  //AES_cbc_decrypt(&buffer[n], entry.buffer, ENTRY_SIZE, &key, iv, MASTER_KEY_SIZE);
+	  AES_cbc_encrypt(&buffer[n], entry.buffer, ENTRY_SIZE, &key, iv, AES_DECRYPT);
 	  print_entry(&entry.entry);
+	  n += ENTRY_SIZE;
 	}
       lseek(secret_file, -BUFFER_SIZE * 2, SEEK_CUR);
     }
